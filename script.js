ae964d9f4c6a424d1c4b78dc30b1e87c356896dd
Ajiri <aj4short@rocketmail.com>
2024-06-21 03:57:53 +0100

    write the logic to play a single round

--- script.js
+++ script.js
@@ -13,3 +13,34 @@ function getHumanChoice(){
     let userChoice = prompt('Rock, Paper, or Scissors')
     return userChoice
 }
+
+function playRound (humanChoice, computerChoice){
+    let humanChoiceToLowerCase = humanChoice.toLowerCase()
+    let lose = 'You lose! Paper beats Rock';
+    let win = 'You Win!';
+    let draw = 'Draw!'
+    if(humanChoiceToLowerCase == computerChoice){
+        console.log(draw)
+    }
+    if(humanChoiceToLowerCase == 'rock' && computerChoice == 'scissors'){
+        console.log(`You Win: ${humanChoiceToLowerCase} crushes ${computerChoice}`)
+        humanScore++
+    } else if(humanChoiceToLowerCase == 'rock' && computerChoice == 'paper'){
+        console.log(`You lose: ${computerChoice} covers ${humanChoiceToLowerCase}`)
+        computerScore++
+    } else if(humanChoiceToLowerCase == 'paper' && computerChoice == 'rock'){
+        console.log(`You Win: ${humanChoiceToLowerCase} covers ${computerChoice}`)
+        humanScore++
+    } else if(humanChoiceToLowerCase == 'paper' && computerChoice == 'scissors'){
+        console.log(`You lose: ${computerChoice} cuts ${humanChoiceToLowerCase}`)
+        computerScore++
+    } else if(humanChoiceToLowerCase == 'scissors' && computerChoice == 'paper'){
+        console.log(`You Win: ${humanChoiceToLowerCase} cuts ${computerChoice}`)
+        humanScore++
+    } else if(humanChoiceToLowerCase == 'scissors' && computerChoice == 'rock'){
+        console.log(`You lose: ${computerChoice} crushes ${humanChoiceToLowerCase}`)
+        computerScore++
+    } else {
+        drawScore++
+    }
+}
