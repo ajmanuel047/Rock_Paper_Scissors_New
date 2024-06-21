e11c874b7ba0bda45d2f492e4291f461457d42b7
Ajiri <aj4short@rocketmail.com>
2024-06-21 03:56:01 +0100

    create variables for human and computerscore and also draw

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+let humanScore = 0;
+let computerScore = 0;
+let drawScore = 0;
+
+
 function getComputerChoice() {
     let arr = ['rock', 'paper', 'scissors'];
     let randomValue = Math.floor(Math.random(arr[arr.length - 1]) * arr.length)
