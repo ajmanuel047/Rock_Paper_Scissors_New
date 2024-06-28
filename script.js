da89d605061dc1354543615575ef4c51590f538b
Ajiri <aj4short@rocketmail.com>
2024-06-28 03:16:27 +0100

    write function to play 4 rounds

--- script.js
+++ script.js
@@ -6,41 +6,61 @@ let drawScore = 0;
 function getComputerChoice() {
     let arr = ['rock', 'paper', 'scissors'];
     let randomValue = Math.floor(Math.random(arr[arr.length - 1]) * arr.length)
+    console.log(arr[randomValue])
     return arr[randomValue]
 }
 
-function getHumanChoice(){
-    let userChoice = prompt('Rock, Paper, or Scissors')
+function getHumanChoice() {
+    let userChoice = prompt('Enter a value')
+    console.log(userChoice)
     return userChoice
 }
 
-function playRound (humanChoice, computerChoice){
-    let humanChoiceToLowerCase = humanChoice.toLowerCase()
-    let lose = 'You lose! Paper beats Rock';
-    let win = 'You Win!';
-    let draw = 'Draw!'
-    if(humanChoiceToLowerCase == computerChoice){
-        console.log(draw)
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+
+
+function playGame() {
+    function playRound(humanChoice, computerChoice) {
+        let humanChoiceToLowerCase = humanChoice.toLowerCase()
+        let lose = 'You lose! Paper beats Rock';
+        let win = 'You Win!';
+        let draw = 'Draw!'
+        if (humanChoiceToLowerCase == computerChoice) {
+            drawScore++
+            console.log(draw)
+        }
+        if (humanChoiceToLowerCase == 'rock' && computerChoice == 'scissors') {
+            console.log(`You Win: ${humanChoiceToLowerCase} crushes ${computerChoice}`)
+            humanScore++
+        } else if (humanChoiceToLowerCase == 'rock' && computerChoice == 'paper') {
+            console.log(`You lose: ${computerChoice} covers ${humanChoiceToLowerCase}`)
+            computerScore++
+        } else if (humanChoiceToLowerCase == 'paper' && computerChoice == 'rock') {
+            console.log(`You Win: ${humanChoiceToLowerCase} covers ${computerChoice}`)
+            humanScore++
+        } else if (humanChoiceToLowerCase == 'paper' && computerChoice == 'scissors') {
+            console.log(`You lose: ${computerChoice} cuts ${humanChoiceToLowerCase}`)
+            computerScore++
+        } else if (humanChoiceToLowerCase == 'scissors' && computerChoice == 'paper') {
+            console.log(`You Win: ${humanChoiceToLowerCase} cuts ${computerChoice}`)
+            humanScore++
+        } else if (humanChoiceToLowerCase == 'scissors' && computerChoice == 'rock') {
+            console.log(`You lose: ${computerChoice} crushes ${humanChoiceToLowerCase}`)
+            computerScore++
+        }
     }
-    if(humanChoiceToLowerCase == 'rock' && computerChoice == 'scissors'){
-        console.log(`You Win: ${humanChoiceToLowerCase} crushes ${computerChoice}`)
-        humanScore++
-    } else if(humanChoiceToLowerCase == 'rock' && computerChoice == 'paper'){
-        console.log(`You lose: ${computerChoice} covers ${humanChoiceToLowerCase}`)
-        computerScore++
-    } else if(humanChoiceToLowerCase == 'paper' && computerChoice == 'rock'){
-        console.log(`You Win: ${humanChoiceToLowerCase} covers ${computerChoice}`)
-        humanScore++
-    } else if(humanChoiceToLowerCase == 'paper' && computerChoice == 'scissors'){
-        console.log(`You lose: ${computerChoice} cuts ${humanChoiceToLowerCase}`)
-        computerScore++
-    } else if(humanChoiceToLowerCase == 'scissors' && computerChoice == 'paper'){
-        console.log(`You Win: ${humanChoiceToLowerCase} cuts ${computerChoice}`)
-        humanScore++
-    } else if(humanChoiceToLowerCase == 'scissors' && computerChoice == 'rock'){
-        console.log(`You lose: ${computerChoice} crushes ${humanChoiceToLowerCase}`)
-        computerScore++
-    } else {
-        drawScore++
+
+    for (let i = 1; i < 5; i++) {
+        playRound(getHumanChoice(), getComputerChoice())
+        console.log(`HumanScore: ${humanScore}`)
+        console.log(`ComputerScore: ${computerScore}`)
+        console.log(`DrawScore: ${drawScore}`)
     }
 }
+
+playGame()
+// console.log(`My Pick: ${getHumanChoice()}`)
+// console.log(`Computer's Pick: ${getComputerChoice()}`)
