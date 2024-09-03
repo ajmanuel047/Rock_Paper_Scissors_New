4b7531308c7ec83167f00ed15ccc8c8927768cb5
Unuafe Ajiri <aj4short@rocketmail.com>
2024-09-03 02:08:29 +0100

    make the paper event listener button functional

--- script.js
+++ script.js
@@ -2,41 +2,49 @@ let humanScore = 0;
 let computerScore = 0;
 let drawScore = 0;
 let rockBtn = document.getElementById('rock');
+let paperBtn = document.getElementById('paper');
 
-rockBtn.addEventListener('click', (e)=> {
+rockBtn.addEventListener('click', (e) => {
     let humanChoice = e.target.id
-    function playRound(humanChoice, computerChoice) {
-        console.log(`My choice : ${humanChoice}`);
-        console.log(`Computer's choice: ${computerChoice}`);
-        let lose = 'You lose! Paper beats Rock';
-        let win = 'You Win!';
-        let draw = 'Draw!'
-        if (humanChoice == computerChoice) {
-            drawScore++
-            console.log(draw)
-        }
-        if (humanChoice == 'rock' && computerChoice == 'scissors') {
-            console.log(`You Win: ${humanChoice} crushes ${computerChoice}`)
-            humanScore++
-        } else if (humanChoice == 'rock' && computerChoice == 'paper') {
-            console.log(`You lose: ${computerChoice} covers ${humanChoice}`)
-            computerScore++
-        } else if (humanChoice == 'paper' && computerChoice == 'rock') {
-            console.log(`You Win: ${humanChoice} covers ${computerChoice}`)
-            humanScore++
-        } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
-            console.log(`You lose: ${computerChoice} cuts ${humanChoice}`)
-            computerScore++
-        } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
-            console.log(`You Win: ${humanChoice} cuts ${computerChoice}`)
-            humanScore++
-        } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
-            console.log(`You lose: ${computerChoice} crushes ${humanChoice}`)
-            computerScore++
-        }
-    }
     playRound(humanChoice, getComputerChoice())
 })
+
+paperBtn.addEventListener('click', (e) => {
+    let humanChoice = e.target.id;
+    playRound(humanChoice, getComputerChoice())
+})
+
+
+function playRound(humanChoice, computerChoice) {
+    console.log(`My choice : ${humanChoice}`);
+    console.log(`Computer's choice: ${computerChoice}`);
+    let lose = 'You lose! Paper beats Rock';
+    let win = 'You Win!';
+    let draw = 'Draw!'
+    if (humanChoice == computerChoice) {
+        drawScore++
+        console.log(draw)
+    }
+    if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        console.log(`You Win: ${humanChoice} crushes ${computerChoice}`)
+        humanScore++
+    } else if (humanChoice == 'rock' && computerChoice == 'paper') {
+        console.log(`You lose: ${computerChoice} covers ${humanChoice}`)
+        computerScore++
+    } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+        console.log(`You Win: ${humanChoice} covers ${computerChoice}`)
+        humanScore++
+    } else if (humanChoice == 'paper' && computerChoice == 'scissors') {
+        console.log(`You lose: ${computerChoice} cuts ${humanChoice}`)
+        computerScore++
+    } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+        console.log(`You Win: ${humanChoice} cuts ${computerChoice}`)
+        humanScore++
+    } else if (humanChoice == 'scissors' && computerChoice == 'rock') {
+        console.log(`You lose: ${computerChoice} crushes ${humanChoice}`)
+        computerScore++
+    }
+}
 
 function getComputerChoice() {
     let arr = ['rock', 'paper', 'scissors'];
